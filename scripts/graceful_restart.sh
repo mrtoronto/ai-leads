0fe16cd1cb5e8b55173ae645a4dc7cb4090b1e75
mrtoronto <matt.toronto97@gmail.com>
2024-08-28 14:11:12 -0700

    updates

--- scripts/graceful_restart.sh
+++ scripts/graceful_restart.sh
@@ -33,32 +33,35 @@ restart_main_service() {
 # Function to restart a worker service
 restart_worker_service() {
     local SERVICE_NAME=$1
-    local INSTANCE_NUMBER=$2
     echo "Restarting $SERVICE_NAME..."
 
     # Stop the service
     sudo systemctl stop "$SERVICE_NAME"
-
-    # Ensure all worker processes are terminated
-    sleep 2  # Wait briefly to ensure the service has stopped
-    local WORKER_NAME="worker-${INSTANCE_NUMBER}-$(hostname)"
-    sudo pkill -f "rq worker.*$WORKER_NAME"
-
-    # Start the service
-    sudo systemctl start "$SERVICE_NAME"
-
-    if sudo systemctl is-active --quiet "$SERVICE_NAME"; then
-        echo "$SERVICE_NAME is now active."
-    else
-        echo "Failed to start $SERVICE_NAME. Please check the logs."
-        sudo journalctl -u "$SERVICE_NAME" -n 20 --no-pager
-    fi
 }
 
 # Function to restart worker services
 restart_workers() {
+    # Stop all worker services
     for i in $(seq 1 $WORKER_COUNT); do
-        restart_worker_service "${WORKER_SERVICE}@$i" "$i"
+        restart_worker_service "${WORKER_SERVICE}@$i"
+    done
+
+    # Ensure all worker processes are terminated
+    sleep 2  # Wait briefly to ensure the services have stopped
+    sudo pkill -f "rq worker"
+
+    # Start all worker services
+    for i in $(seq 1 $WORKER_COUNT); do
+        sudo systemctl start "${WORKER_SERVICE}@$i"
+        # Wait for the service to start
+        sleep 5
+
+        if sudo systemctl is-active --quiet "${WORKER_SERVICE}@$i"; then
+            echo "${WORKER_SERVICE}@$i is now active."
+        else
+            echo "Failed to start ${WORKER_SERVICE}@$i. Please check the logs."
+            sudo journalctl -u "${WORKER_SERVICE}@$i" -n 20 --no-pager
+        fi
     done
 }
 
